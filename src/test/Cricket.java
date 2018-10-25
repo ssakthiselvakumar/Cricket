2e2047fb7461412a4e9f9119df9c6be5bc419c70
ssakthiselvakumar <sakthi050694@gmail.com>
2018-10-25 16:12:16 +0530

    hihi

--- src/test/Cricket.java
+++ src/test/Cricket.java
@@ -1,5 +1,17 @@
 package test;
 
 public class Cricket {
+	public static void main(String[] args) {
+		System.out.println("over");
+		
+	}
+	public void OneDay() {
+		System.out.println("50 over");
+
+	}
+	public void OneDays() {
+		System.out.println("50 over");
+
+	}
 
 }
